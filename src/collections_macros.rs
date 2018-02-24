b11884fcdeafc45536e9ca62d9e155140620d119
Fangyuanziti <tiziyuanfang@gmail.com>
2018-02-24 17:10:24 +0800

    use `maplit` and `cute` to build a collections helper macro group

---
+++ src/collections_macros.rs
@@ -0,0 +1,18 @@
+pub use maplit::*;
+pub use cute::*;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+
+        let v = vec![1, 2 ,3];
+        let map = hashmap!(
+            1 => "1".to_owned(),
+            2 => "2".to_owned(),
+            3 => "3".to_owned(),
+        );
+
+        assert_eq!(map, c![i => i.to_string(), for i in v])
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
+#[macro_use]
+#[allow(unused_imports)]
+extern crate maplit;
+
+#[macro_use]
+#[allow(unused_imports)]
+extern crate cute;
+
 mod result_ext;
 mod hashmap_ext;
+mod collections_macros;
 
 pub use result_ext::SResultExt;
 pub use hashmap_ext::SHashMapExt;
+pub use collections_macros::*;
